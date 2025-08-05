614328e74892ea99b4b238f97ea78202fc51ad45
Mohammad Faisal <faisaltinza@gmail.com>
2025-08-05 14:13:48 +0530

    Add files via upload

--- simplecalc.js
+++ simplecalc.js
@@ -1,21 +1,53 @@
 const buttonEl = document.querySelectorAll('button');
 const displayE1 = document.getElementById('display');
 let calculated = false;
+let Oprator = false;
 
 buttonEl.forEach((e)=>{
     e.addEventListener('click', function () {
         const buttonValue = (e.innerHTML)
         if (buttonValue === 'C'){
             calculated = false;
+            Oprator = false;
             clearResults()
         }else if(buttonValue === '='){
-            calculated = true;
-            calculateResult()
+            try {calculated = true;
+                Oprator = true;
+                calculateResult()
+                // risky code
+            } catch(error) {
+                Oprator = true;
+                displayE1.value = 'error';
+                // error aaya to yeh chalega
+            }
+
         }else if(buttonValue === '←'){
+            if(['+', '-', '*', '/'].includes(displayE1.value.slice(-1))){
+                Oprator = false
+            }
             clearlastDigit()
+            if(['+', '-', '*', '/'].includes(displayE1.value.slice(-1))) {
+                Oprator = true
+            }
+
+        }else if(['+', '-', '*', '/'].includes(buttonValue)){
+            if(displayE1.value === ""){
+                console.log('empty')
+                return;
+            }
+            else if(['+', '-', '*', '/'].includes(displayE1.value.slice(-1))){
+                console.log('Not allowed')
+            }
+            if (Oprator === false){
+                appendValue(buttonValue)
+                Oprator = true
+            }
+            else{
+                console.log(Oprator)}
         }
         else {
             appendValue(buttonValue)
+            Oprator = false
         }
     } )
 })
@@ -29,7 +61,7 @@ function calculateResult() {
         displayE1.value = "Enter Some figures"
     }
     displayE1.value = eval(displayE1.value)
-
+    Oprator = true
 }
 function appendValue(buttonValue) {
     if (calculated === true){
@@ -46,3 +78,4 @@ function clearlastDigit() {
         calculated = true
     }
 }
+
